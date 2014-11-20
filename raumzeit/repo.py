f219b8f00e31acf460442c961a88d02954cbc46d
Alec Hanefeld <alec@hanefeld.eu>
2014-11-20 16:13:23 +0100

    iter_all, get_one and slugify

--- raumzeit/repo.py
+++ raumzeit/repo.py
@@ -1,7 +1,10 @@
 import py2neo
+import re
+import hashlib
+from unicodedata import normalize as uni_normalize
 
+_punct_re = re.compile(r"""[\t !"#$%&\'()*\-/<=>?@\[\\\]^_`{|},.]+""")
 
-#
 class Repository(object):
 
     def __init__(self):
@@ -22,16 +25,57 @@ class Repository(object):
     def create(self, collection, **kwargs):
         raise NotImplementedError
 
+    @staticmethod
+    def slugify(text, delim='-', hash_obj=False, hash_chars=10, punct_re=_punct_re):
+        def calc_hash(obj):
+            bytes = uni_normalize('NFKD', str(obj)).encode('ascii', 'ignore')
+            hash_key = hashlib.sha224(bytes).hexdigest()
+            return hash_key
+
+        result = []
+        for word in punct_re.split(text.lower()):
+            word = uni_normalize('NFKD', word).encode('ascii', 'ignore')
+            if word:
+                result.append(word.decode('utf-8'))
+
+        if hash_obj:
+            result.append(calc_hash(hash_obj)[:hash_chars])
+
+        return delim.join(result)
+
 class NeoRepository(Repository):
 
     def __init__(self, graph):
         self._graph = graph
 
     def iter_all(self, label):
+        """ Iterate over all entities for a given label """
         match_label_q = """MATCH (n:{l}) RETURN n""".format(l=label)
         record_stream = self._graph.cypher.stream(match_label_q)
         for record in record_stream:
-            yield _record_to_dict(record)
+            yield self._record_to_dict(record)
+
+    def get_one(self, label, query):
+        """ Get a unique enity that matches the query dictionary """
+        param_str = self._dict_to_param_q(query)
+        match_one_q = """MATCH (n:{l} {q}) RETURN n""".format(l=label, q=param_str)
+        rec_list = self._graph.cypher.execute(match_one_q, parameters=query)
+        num_results = len(rec_list)
+        if num_results == 0:
+            raise KeyError('Not found')
+        elif num_results > 1:
+            raise KeyError('Not unique')
+        else:
+            rec = rec_list[0]
+            return self._record_to_dict(rec)
+
+    def create(self, label, props):
+        """ Create a new sluggable entity """
+        pass
+
+    def get_or_create(self, label, props):
+        """ Get or create a value node. """
+        pass
         
 
     @classmethod
@@ -55,8 +99,18 @@ class NeoRepository(Repository):
     @classmethod
     def _node_to_dict(cls, node):
         node_dict = node.properties.copy()
-        node_dict['labels'] = node.labels
+        node_dict['labels'] = set(node.labels)
         return node_dict
         
+    @classmethod
+    def _dict_to_param_q(cls, dct):
+        """ Compiles a string for parameterized queries. """
+        q = '{'
+        for key, value in dct.items():
+            q += key + ': ' + '{' + key + '}' + ', '
+        q = q[:-2] #remove last ', '
+        q += '}'
+        return q
 
-#    def get(self, label, slug):
+    #def create(self, label, **kwargs):
+
--- tests/test_repository.py
+++ tests/test_repository.py
@@ -6,6 +6,7 @@ import requests
 
 host = 'localhost'
 neo_uri = 'http://{host}:7474/db/data/'.format(host=host)
+graph = py2neo.Graph(neo_uri)
 
 def clear_db(host=host):
     resp = requests.post('http://{host}:7474/graphaware/resttest/clear'.format(host=host))
@@ -14,7 +15,7 @@ def clear_db(host=host):
     else:
         raise ValueError(resp.status)
 
-def assert_same_graph_state(cypher, host=host):
+def is_same_graph(cypher, host=host):
     body = json.dumps({"cypher": cypher})
     headers= {'content-type': 'application/json'}
     url = 'http://{host}:7474/graphaware/resttest/assertSameGraph'.format(host=host)
@@ -29,7 +30,7 @@ def test_db_testing():
     g = py2neo.Graph(neo_uri)
     create_q = "CREATE (one:Person {name:'One'})-[:FRIEND_OF]->(two:Person {name:'Two'})"
     g.cypher.execute(create_q)
-    assert assert_same_graph_state(create_q)
+    assert is_same_graph(create_q)
 
 
 def test_repo_signatures():
@@ -52,7 +53,7 @@ def test_repo_signatures():
         r.create('location', name='renate')
     
 
-def test_casts_():
+def test_casts():
     
     node = py2neo.Node('LableA', 'LableB', prop_a=1, prop_b='foo')
     node_dict = NeoRepository._node_to_dict(node)
@@ -73,12 +74,75 @@ def test_casts_():
     assert rec_dict['prop_a'] == 1
     assert rec_dict['prop_b'] == 'foo'
 
+def test_create():
+    clear_db()
+    repo = NeoRepository(graph)
+    
+    #repo.create('Location', {'name': 'renate'})
+
+    #assert is_same_graph("""CREATE (l:Location {name: 'renate'})""")
+
+def test_iter_all():
+    clear_db()
+
+    
+    graph.cypher.execute("""CREATE (n: Location {name: 'renate'})""")
+    graph.cypher.execute("""CREATE (n: Location {name: 'kater'})""")
+    graph.cypher.execute("""CREATE (n: Location {name: 'kater2'})""")
+    graph.cypher.execute("""CREATE (n: Other {name: 'foobar'})""")
+
+    repo = NeoRepository(graph)
+    locations = list(repo.iter_all('Location'))
+    assert len(locations) == 3
+    for each in locations:
+        assert len(each.items()) == 2
+        assert 'name' in each.keys()
+        assert each['labels'] == {'Location'}
+
+
+def test_compile_query():
+    d = {'a': 123, 'b': 'foo'}
+    s = NeoRepository._dict_to_param_q(d)
+
+    # our dict is accessed randomly so we test both orderings
+    assert (s == """{a: {a}, b: {b}}""" or s == """{b: {b}, a: {a}}""")
+
+def test_get_one():
+    clear_db()
+
+    graph.cypher.execute("""CREATE (n: Location {name: 'renate'})""")
+    graph.cypher.execute("""CREATE (n: Location {name: 'kater'})""")
+    graph.cypher.execute("""CREATE (n: Location {name: 'kater'})""")
+    graph.cypher.execute("""CREATE (n: Location {name: 'kater2'})""")
+    graph.cypher.execute("""CREATE (n: Other {name: 'foobar'})""")
+
+    repo = NeoRepository(graph)
+    one = repo.get_one('Location', {'name': 'renate'})
+    assert one['name'] == 'renate'
+    assert one['labels'] == {'Location'}
+
+
+    with pytest.raises(KeyError) as e:
+        two = repo.get_one('Location', {'name': 'kater'})
+    assert 'Not unique' in str(e.value)
+
+    with pytest.raises(KeyError) as e:
+        two = repo.get_one('Location', {'name': 'notexisting'})
+    assert 'Not found' in str(e.value)
+
+def test_generate_slug():
+    names = ['Kater Holzig', 'Möp']
+    slugs = [NeoRepository.slugify(n) for n in names]
+    assert slugs == ['kater-holzig', 'mop']
+
+    slug = Repository.slugify('Kater Holzig', hash_obj={'a': 123, 'b': 'ööö'})
+    assert slug == 'kater-holzig-ef5f1f3a92'
+
 # def test_create():
-#     g = py2neo.Graph(neo_uri)
-#     assert g.bound
+#     clear_db()
 
-#     repo = NeoRepository(g)
-#     repo.create('Location', {'name': 'renate'})
+#     repo = NeoRepository(graph)
+#     props = {'name': 'Kater Holzig', 'desc': 'Some Text'}
+#     created_a = repo.create('Location', props)
 
-
-
+#     assert created['slug'] == 
